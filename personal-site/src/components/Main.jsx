10116cc0e88a1f9b9a57a3a1d325172b3aed1566
Victor Montoya <victormontoya@Victors-MacBook-Pro.local>
2019-07-29 17:24:18 -0600

    modified main component, added linkedin github links

--- personal-site/src/components/Main.jsx
+++ personal-site/src/components/Main.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleDown, } from '@fortawesome/free-solid-svg-icons'
 
 function Main(){
     return(
@@ -9,7 +13,21 @@ function Main(){
                 <div>Software Developer</div>
             </div>
             <div className="social-media">
-                social media
+                <span>
+                    <a href="https://github.com/thatvictor7"> 
+                        <FontAwesomeIcon color="white" icon={faGithub} />
+                    </a>
+                </span>
+                <span>
+                    <a href="https://www.linkedin.com/in/victor-montoya/">
+                        <FontAwesomeIcon color="white" icon={faLinkedin} />
+                    </a>
+                </span>
+            </div>
+            <div className="down-arrow-container">
+                <div>Learn More</div>
+                <FontAwesomeIcon className="" color="white" size="2x" icon={faAngleDoubleDown} />
+                {/* <Image className="down-arrow" src={require("./assets/arrow_down_white.png")} /> */}
             </div>
         </div>
     )
